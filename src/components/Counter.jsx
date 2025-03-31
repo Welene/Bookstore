89709d24d6a90d71d351d1b9a02e17937fcd1ea6
Welene <helenetheo@live.no>
2025-03-31 15:35:58 +0200

    Made it so that you cannot have negative numbers when adding books

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { useState } from 'react';
 
 function AmountContainer() {
+	const [amount, setAmount] = useState(0);
+	// setter startamount på knappen under boken her, 0
+
+	const remove = () => {
+		if (amount > 0) setAmount(amount - 1);
+	};
+	const add = () => {
+		setAmount(amount + 1);
+	};
 	return (
 		<section className="bookListSection__amountContainer">
-			<button className="bookListSection__amountContainer-add">-</button>
-			<p className="bookListSection__amountContainer-number">0</p>
-			<button className="bookListSection__amountContainer-remove">
+			<button
+				className="bookListSection__amountContainer-add"
+				onClick={remove}>
+				-
+			</button>
+			<p className="bookListSection__amountContainer-number">{amount}</p>
+			<button
+				className="bookListSection__amountContainer-remove"
+				onClick={add}>
 				+
 			</button>
 		</section>
